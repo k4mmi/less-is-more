3eaa53a5c2a6bd4415d3b240e19c811107897165
k4mmi :3 <114113059+k4mmi@users.noreply.github.com>
2024-12-16 15:39:51 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // nebyl v podstatě žádný čas na přidání jějakých JS vylepšení, takže...
 
 // Version
-const version = "v0.1.0-beta";
+const version = "v0.1.1-beta";
 
 // Set version in some html tag
 document.getElementById("version").innerHTML = "Verze: " + version;
